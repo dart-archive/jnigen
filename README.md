42dbb42a877303ab2aa3ac34401ec6c5b75798a3
Daco Harkes <dacoharkes@google.com>
2022-05-31 11:04:03 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Experimental generator for FFI+JNI bindings.
+
+This package will generate JNI code to invoke Java from C, and Dart FFI bindings to invoke this C code.
+This enables calling Java code from Dart.
+
+This is a GSoC 2022 project.
---
+++ test/my_test.dart
@@ -0,0 +1,13 @@
+// Copyright (c) 2022, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:test/test.dart';
+import 'package:jnigen/jnigen.dart';
+
+void main() {
+  test('dummy test', () {
+    final result = mySum(2, 40);
+    expect(result, 42);
+  });
+}
