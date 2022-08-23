71d865d2e0c1cd590bef642336584263449c4f12
Mahesh Hegde <net.mahesh29@gmail.com>
2022-08-23 09:06:52 +0530

    add copyright notices to files in jni_gen

--- jni_gen/lib/src/bindings/symbol_resolver.dart
+++ jni_gen/lib/src/bindings/symbol_resolver.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2022, the Dart project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 // A symbol resolver is useful mainly to convert a fully qualified name to
 // a locally meaningful name, when creating dart bindings
 
--- jni_gen/test/jackson_core_test/generate.dart
+++ jni_gen/test/jackson_core_test/generate.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2022, the Dart project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 import 'package:jni_gen/jni_gen.dart';
 import '../test_util/test_util.dart';
 
