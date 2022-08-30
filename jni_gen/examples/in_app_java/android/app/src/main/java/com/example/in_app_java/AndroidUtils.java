4f118d81e3171082cc68b370677140758a6748e8
Mahesh Hegde <net.mahesh29@gmail.com>
2022-08-30 21:12:00 +0530

    fix CI errors

--- jni_gen/examples/in_app_java/android/app/src/main/java/com/example/in_app_java/AndroidUtils.java
+++ jni_gen/examples/in_app_java/android/app/src/main/java/com/example/in_app_java/AndroidUtils.java
@@ -1,15 +1,12 @@
 package com.example.in_app_java;
 
-import androidx.annotation.Keep;
 import android.app.Activity;
-import android.content.Context;
 import android.widget.Toast;
+import androidx.annotation.Keep;
 
 @Keep
 public class AndroidUtils {
-  static void showToast(
-      Activity mainActivity, CharSequence text, int duration) {
-	mainActivity.runOnUiThread(() ->
-		Toast.makeText(mainActivity, text, duration).show());
+  static void showToast(Activity mainActivity, CharSequence text, int duration) {
+    mainActivity.runOnUiThread(() -> Toast.makeText(mainActivity, text, duration).show());
   }
 }
