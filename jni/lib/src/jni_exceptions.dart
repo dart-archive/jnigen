02e3f666489e558e8f12f7584e6bb3d9ecffd874
Mahesh Hegde <net.mahesh29@gmail.com>
2022-09-08 10:30:45 +0530

    Copyright notices in source files

--- jni/lib/src/jni_exceptions.dart
+++ jni/lib/src/jni_exceptions.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2022, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 import 'dart:ffi';
 
 import 'third_party/jni_bindings_generated.dart';
