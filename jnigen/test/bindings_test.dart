afc550a231b109f7de61da7b21344b09fe6f2f9e
Hossein Yousefi <yousefi@google.com>
2023-02-01 17:16:31 +0100

    run ci

--- jnigen/test/bindings_test.dart
+++ jnigen/test/bindings_test.dart
@@ -73,6 +73,8 @@ Future<void> setupDylibsAndClasses() async {
     '-s',
     join(kotlinTest, 'src'),
   ]);
+  await runCommand('mvn', ['clean'], workingDirectory: kotlinTestKotlin);
+  print('so far so good!');
   await runCommand('mvn', ['package'], workingDirectory: kotlinTestKotlin);
   // Jar including Kotlin runtime and dependencies.
   final kotlinTestJar =
