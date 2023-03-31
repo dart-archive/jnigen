9df3b8b0b288b82d8da3787e45bbc4ad3cb49ebe
Mahesh Hegde <46179734+mahesh-hegde@users.noreply.github.com>
2023-03-31 19:27:41 +0530

    Move summarizer invocation into summary.dart for better testability.

--- jnigen/lib/src/generate_bindings.dart
+++ jnigen/lib/src/generate_bindings.dart
@@ -23,83 +23,12 @@ Future<void> generateJniBindings(Config config) async {
 
   await buildSummarizerIfNotExists();
 
-  final summarizer = SummarizerCommand(
-    sourcePath: config.sourcePath,
-    classPath: config.classPath,
-    classes: config.classes,
-    workingDirectory: config.summarizerOptions?.workingDirectory,
-    extraArgs: config.summarizerOptions?.extraArgs ?? const [],
-    backend: config.summarizerOptions?.backend,
-  );
-
-  // Additional sources added using maven downloads and gradle trickery.
-  final extraSources = <Uri>[];
-  final extraJars = <Uri>[];
-  final mavenDl = config.mavenDownloads;
-  if (mavenDl != null) {
-    final sourcePath = mavenDl.sourceDir;
-    await Directory(sourcePath).create(recursive: true);
-    await MavenTools.downloadMavenSources(
-        MavenTools.deps(mavenDl.sourceDeps), sourcePath);
-    extraSources.add(Uri.directory(sourcePath));
-    final jarPath = mavenDl.jarDir;
-    await Directory(jarPath).create(recursive: true);
-    await MavenTools.downloadMavenJars(
-        MavenTools.deps(mavenDl.sourceDeps + mavenDl.jarOnlyDeps), jarPath);
-    extraJars.addAll(await Directory(jarPath)
-        .list()
-        .where((entry) => entry.path.endsWith('.jar'))
-        .map((entry) => entry.uri)
-        .toList());
-  }
-  final androidConfig = config.androidSdkConfig;
-  if (androidConfig != null && androidConfig.addGradleDeps) {
-    final deps = AndroidSdkTools.getGradleClasspaths(
-      configRoot: config.configRoot,
-      androidProject: androidConfig.androidExample ?? '.',
-    );
-    extraJars.addAll(deps.map(Uri.file));
-  }
-  if (androidConfig != null && androidConfig.versions != null) {
-    final versions = androidConfig.versions!;
-    final androidSdkRoot =
-        androidConfig.sdkRoot ?? AndroidSdkTools.getAndroidSdkRoot();
-    final androidJar = await AndroidSdkTools.getAndroidJarPath(
-        sdkRoot: androidSdkRoot, versionOrder: versions);
-    if (androidJar != null) {
-      extraJars.add(Uri.directory(androidJar));
-    }
-  }
-
-  summarizer.addSourcePaths(extraSources);
-  summarizer.addClassPaths(extraJars);
-
-  Process process;
-  Stream<List<int>> input;
-  try {
-    process = await summarizer.runProcess();
-    input = process.stdout;
-  } on Exception catch (e) {
-    log.fatal('Cannot obtain API summary: $e');
+  final classes = await getSummary(config);
+  if (classes == null) {
+    log.fatal("Error occurred while parsing inputs.");
     return;
   }
-  final errorLog = StringBuffer();
-  collectOutputStream(process.stderr, errorLog);
-  final stream = const JsonDecoder().bind(const Utf8Decoder().bind(input));
-  dynamic json;
-  try {
-    json = await stream.single;
-  } on Exception catch (e) {
-    printError(errorLog);
-    log.fatal('Cannot parse summary: $e');
-    return;
-  }
-  if (json == null) {
-    log.fatal('Expected JSON element from summarizer.');
-    return;
-  }
-  final list = json as List;
-  final classes = Classes.fromJson(list);
+
   final cBased = config.outputConfig.bindingsType == BindingsType.cBased;
   classes
     ..accept(Excluder(config))
