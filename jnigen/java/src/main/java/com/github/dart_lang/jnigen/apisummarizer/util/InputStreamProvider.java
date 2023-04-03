aeacb69937ad19f77eec7237ac6214ddf08b4650
Mahesh Hegde <46179734+mahesh-hegde@users.noreply.github.com>
2023-04-03 19:33:16 +0530

    Add doc comment

--- jnigen/java/src/main/java/com/github/dart_lang/jnigen/apisummarizer/util/InputStreamProvider.java
+++ jnigen/java/src/main/java/com/github/dart_lang/jnigen/apisummarizer/util/InputStreamProvider.java
@@ -3,7 +3,9 @@ package com.github.dart_lang.jnigen.apisummarizer.util;
 import java.io.InputStream;
 
 public interface InputStreamProvider {
+  /** Return the input stream, initializing it if needed. */
   InputStream getInputStream();
 
+  /** close the underlying InputStream. */
   void close();
 }
