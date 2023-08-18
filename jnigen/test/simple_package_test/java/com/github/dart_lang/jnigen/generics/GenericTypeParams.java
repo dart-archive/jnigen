50bff1bd3b249570271c950737f46474a0188720
Mahesh Hegde <46179734+mahesh-hegde@users.noreply.github.com>
2023-08-18 16:17:11 +0530

    Fix java forma

--- jnigen/test/simple_package_test/java/com/github/dart_lang/jnigen/generics/GenericTypeParams.java
+++ jnigen/test/simple_package_test/java/com/github/dart_lang/jnigen/generics/GenericTypeParams.java
@@ -1,6 +1,5 @@
 package com.github.dart_lang.jnigen.generics;
 
-public class GenericTypeParams<S extends CharSequence, K extends GenericTypeParams<S, K>> {
-}
+public class GenericTypeParams<S extends CharSequence, K extends GenericTypeParams<S, K>> {}
 
 // TODO: Add more cases of generic parameters
